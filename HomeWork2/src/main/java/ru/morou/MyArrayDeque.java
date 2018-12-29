10df02d2bc0fc5c749aefd1b86ac32e0441a26f8
Mori Morou <mori_morou@yahoo.com>
2018-12-29 17:27:23 +0300

    added my home work

---
+++ HomeWork2/src/main/java/ru/morou/MyArrayDeque.java
@@ -0,0 +1,120 @@
+package ru.morou;
+
+/**
+ * Hello world!
+ *
+ */
+public class MyArrayDeque<Item> {
+
+    private static final int SIZE_INCREASE_RATIO = 2;
+
+    private Object[] queue;
+
+    private int start;
+
+    private int end;
+
+    private int size;
+
+    public MyArrayDeque() {
+        start = 0;
+        end = 0;
+        size = 0;
+        queue = new Object[1];
+    }
+
+    // метод возвращает размер очереди
+    public int getSize() {
+        return size;
+    }
+
+    // метод проверяет очередь на заполненность
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public void resize () {
+        int newSize;
+        int tempIndex = 1;
+
+        if (size == 0)
+            newSize = SIZE_INCREASE_RATIO;
+        else
+            newSize = size * SIZE_INCREASE_RATIO;
+
+        Object[] temp = new Object[newSize];
+
+        if (size > 0) {
+            if (end < start) {
+                for (int i = start; i < queue.length; i++, tempIndex++)
+                    temp[tempIndex] = queue[i];
+
+                for (int i = 0; i <= end; i++, tempIndex++)
+                    temp[tempIndex] = queue[i];
+            } else
+                for (int i = start; i < end; i++, tempIndex++)
+                    temp[tempIndex] = queue[i];
+        } else {
+            start = 0;
+            end = -1;
+        }
+        queue = temp;
+    }
+
+
+    public void insertLeft (Item item) {
+        if (queue.length == size)
+            resize();
+
+        if (start > 0)
+            start--;
+        else
+            start = queue.length - 1;
+
+        queue[start] = item;
+        size++;
+
+        if (size == 1)
+            end = start;
+    }
+
+    public void inserRight (Item item) {
+        if (queue.length == size)
+            resize();
+
+        if (end == queue.length - 1)
+            end = 0;
+        else
+            end++;
+
+        queue[start] = item;
+        size++;
+
+        if (size == 1)
+            start = end;
+    }
+
+    public Object peekLeft () {
+        Object res;
+
+        if (size == 0) {
+            System.err.println("Queue empty");
+            res = null;
+        } else {
+            res = queue[start];
+        }
+        return res;
+    }
+
+    public Object peekRight () {
+        Object res;
+
+        if (size == 0) {
+            System.err.println("Queue empty");
+            res = null;
+        } else {
+            res = queue[end];
+        }
+        return res;
+    }
+}
