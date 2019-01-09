8d2ed78120595516a1acd9bcee83a555983567c0
Mori Morou <mori_morou@yahoo.com>
2019-01-09 14:50:18 +0300

    Added Homework 4

---
+++ HomeWork_4/src/Generic/LinkIterator.java
@@ -0,0 +1,52 @@
+package Generic;
+
+import java.util.Iterator;
+import java.util.function.Consumer;
+
+// Методы в этом классе - получение эелемента и проверка наличия следующего элемента
+public class LinkIterator implements Iterator {
+    private Link current;
+    private Link previous;
+    private LinkedList linkedList;
+
+    public LinkIterator(LinkedList linkedList) {
+        this.linkedList = linkedList;
+        this.reset();
+    }
+
+    public void reset(){
+        current = linkedList.getFirst();
+        previous = null;
+    }
+
+    @Override
+    public boolean hasNext(){
+        return current.getNext() != null;
+    }
+
+    @Override
+    public Object next() {
+        nextLink();
+        return getCurrent();
+    }
+
+    @Override
+    public void remove() {
+
+    }
+
+    @Override
+    public void forEachRemaining(Consumer action) {
+
+    }
+
+    private void nextLink(){
+        previous = current;
+        current = current.getNext();
+    }
+
+    private Link getCurrent(){
+        return current;
+    }
+
+}
